edc11c50563083a4b7854e71f04fc03d365ef590
Suraj Shrestha <sshrestha@salesforce.com>
2024-04-27 23:14:35 -0400

    fixing scripts path

--- alb.tf
+++ alb.tf
@@ -51,6 +51,11 @@ resource "aws_lb_target_group" "saa-public-lb-target-group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.saa_vpc.id
+  health_check {
+    interval = 10
+    healthy_threshold = 3
+    unhealthy_threshold = 2
+  }
 }
 
 
